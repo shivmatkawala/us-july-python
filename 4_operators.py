f7f2b7bbc5e047612a55dbf3ec01321b888106e0
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-07-16 20:30:23 +0530

    operators list

---
+++ 4_operators.py
@@ -0,0 +1,57 @@
+# Operators:
+    # Expression 
+    # x + 5 = 10  
+        # Operators: +, =
+        # Operands: 
+                # Variables: x
+                # Constants: 5, 1
+
+    # Any expression is formed using operators and operands.
+
+    # Types of Operators :
+        # Arithmetic Operators:
+            # Addition +
+            # Substraction -
+            # Multiplication *
+            # True Division /
+            # Floor Division //
+            # Exponentiation (Power) **
+            # Modulus (Mod) %
+
+        # Comparision Operators:
+            # Greter than >
+            # Lesser than <
+            # Greter than or equal to >=
+            # Lesser than or equat to <=
+            # Not equal to !=,  <>
+            # Equal to ==
+        
+        # Assignement Operators:
+            # Assign =
+            # Add and Assign +=
+            # Substract and Assign -=
+            # Multiply and assign *=
+            # True Divide and assign /=
+            # Floor divide and assign //=
+            # exponentition and assign **=
+            # mod and assign %=
+
+        # Indentity Operators:
+            # is
+            # is not
+
+        # Membership Operators:
+            # in
+            # not in
+
+        # Logical Operators:
+            # and 
+            # or
+            # not
+
+        # Ternary Operator:
+
+        # Bitwise Operators:
+            # & (AND)
+            # | (OR)
+            # ^ (XOR)
